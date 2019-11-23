54347218e6ff3bc129c592dd4f2f7d5cfe69f4a1
kasidit <kasidittom@gmail.com>
2019-11-23 17:11:10 +0700

    commit

---
+++ src/th/co/cdgs/bean/CustomerDto.java
@@ -0,0 +1,54 @@
+package th.co.cdgs.bean;
+
+public class CustomerDto {
+	
+	private Long customerId;
+	private String fullName;
+	private String address;
+	private String tel;
+	private String email;
+	private String firstName;
+	private String lastName;
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ src/th/co/cdgs/bean/WorkshopApplication.java
@@ -0,0 +1,9 @@
+package th.co.cdgs.bean;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class WorkshopApplication extends Application {
+
+}
